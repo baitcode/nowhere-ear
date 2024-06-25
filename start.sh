6dd6ed26f4c532b445e64d7b2eec1b47d49f6cca
ekaterina.loshchinina@gmail.com <katerinaloschinina@Katerinas-MacBook-Air.local>
2024-06-25 18:27:06 +0100

    try bash script

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 sudo truncate -s 0 /var/log/syslog
-source /home/pi/.nvm/nvm.sh
-. /home/pi/kinki-yokai/
 nvm alias default 10
+. /home/pi/kinki-yokai/
 npm run prod & python3 ./sound/run_sound.py
