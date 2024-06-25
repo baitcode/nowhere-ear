ef8c53bbb12dd94cb0a2b2a8804c942785c6f143
ekaterina.loshchinina@gmail.com <katerinaloschinina@Katerinas-MacBook-Air.local>
2024-06-25 18:54:00 +0100

    try

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 sudo truncate -s 0 /var/log/syslog
 . /home/pi/.nvm/nvm.sh
 nvm use default 10
-sudo amixer cset numid=3 99%
+amixer cset numid=3 99%
 . /home/pi/kinki-yokai/
 npm run prod & python3 /home/pi/kinki-yokai/sound/run_sound.py
