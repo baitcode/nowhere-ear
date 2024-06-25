912f1873129c49592c5611a72449d0a9ab8a442a
ekaterina.loshchinina@gmail.com <katerinaloschinina@Katerinas-MacBook-Air.local>
2024-06-25 18:32:50 +0100

    try

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo truncate -s 0 /var/log/syslog
-source /home/pi/.nvm/nvm.sh
+. /home/pi/.nvm/nvm.sh
 nvm use default 10
 . /home/pi/kinki-yokai/
 npm run prod & python3 ./sound/run_sound.py
